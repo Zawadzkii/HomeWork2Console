d43b66225485505662a5a3475e230cf0c863500b
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-03-21 17:59:32 +0100

    Add project files.

---
+++ HomeWork2Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		// Declaration of three variables of type char
+		char first = 'A';
+		char second = 'B';
+		char third = 'C';
+
+		// Displaying variables in the original order
+		Console.WriteLine("Original order:");
+		Console.WriteLine("First variable: " + first);
+		Console.WriteLine("Second variable: " + second);
+		Console.WriteLine("Third variable: " + third);
+
+		// Displaying variables in reverse order
+		Console.WriteLine("\nReverse order:");
+		Console.WriteLine("Third variable: " + third);
+		Console.WriteLine("Second variable: " + second);
+		Console.WriteLine("First variable: " + first);
+
+		Console.ReadKey();
+	}
+}
